2c19f188c2b36997f01ff23e177cbf4545a3ca25
OVEGERO <rat4355@gmail.com>
2025-09-23 22:55:25 -0500

    refactor: organize module imports and providers for clarity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,49 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// Builder Pattern
+
+// Builder Pattern - Vehículos
 import { VehiculoController } from './patterns/builder/controllers';
 import { VehiculoServicio } from './patterns/builder/services';
 import { VehiculoRepositorioPrisma } from './patterns/builder/repositories';
-// Factory Pattern
+
+// Factory Pattern - Activos Fijos
 import { ActivoController } from './patterns/factory/controllers';
 import { ActivoServicio } from './patterns/factory/services';
 import { ActivoRepositorioPrisma } from './patterns/factory/repositories';
+import { ActivoFijoFactorySimple } from './patterns/factory/factories';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      expandVariables: true,
-    }),
+  imports: [],
+  controllers: [
+    AppController, 
+    VehiculoController,
+    ActivoController,
   ],
-  controllers: [AppController, VehiculoController, ActivoController],
   providers: [
     AppService,
-    { provide: 'IVehiculoRepositorio', useClass: VehiculoRepositorioPrisma },
-    { provide: 'IVehiculoServicio', useClass: VehiculoServicio },
-    { provide: 'IActivoRepositorio', useClass: ActivoRepositorioPrisma },
-    { provide: 'IActivoServicio', useClass: ActivoServicio },
+    // Builder Pattern providers
+    {
+      provide: 'IVehiculoServicio',
+      useClass: VehiculoServicio,
+    },
+    {
+      provide: 'IVehiculoRepositorio',
+      useClass: VehiculoRepositorioPrisma,
+    },
+    // Factory Pattern providers
+    {
+      provide: 'IActivoServicio',
+      useClass: ActivoServicio,
+    },
+    {
+      provide: 'IActivoRepositorio',
+      useClass: ActivoRepositorioPrisma,
+    },
+    {
+      provide: 'IActivoFijoFactory',
+      useClass: ActivoFijoFactorySimple,
+    },
   ],
 })
 export class AppModule {}
